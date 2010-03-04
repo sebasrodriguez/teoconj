e44efdb86c3e3fbd8e3e928c397addc51d9b41dd
sebasrodriguez <sebasrodriguez@861ad466-0edf-11df-a223-d798cd56f61e>
2010-03-04 02:51:14 +0000

    El remove estaba implementado por pablo finalmente, el tema es que descubri un bug en el mismo, al crear un conjunto del estilo c1 = {1 , 2} , si haciamos un remove del elemento 1 y luego intentabamos hacer un remove del elemento 2 se caia la aplicacion, la razon de esto es que nuestra lista de ABB's el elemento c1 seguia apuntando al espacio de memoria donde estaba el 1 , al no ser actualizado eso quedaba una referencia perdida. Lo solucione llamando a un metodo que actualiza la referencia al nuevo elemento base del ABB, de todas formas hay que hacer un poco de refactoring en general ya que quedo medio desprolijo.
    PD: este comportamiento esta igual en el obligatorio de p3 por lo cual entregamos ese bug y capaz que eso fue lo q nos hizo perder 5 puntos.

--- source/lib/arbolint.cpp
+++ source/lib/arbolint.cpp
@@ -1,10 +1,12 @@
 #include "arbolint.h"
 
-void ArbolIntCreate(ArbolInt &a){
+void ArbolIntCreate(ArbolInt &a)
+{
     a = NULL;
 }
 
-ArbolInt ArbolIntCons(int info, ArbolInt hizq, ArbolInt hder){
+ArbolInt ArbolIntCons(int info, ArbolInt hizq, ArbolInt hder)
+{
     ArbolInt a = new NodoArbolInt;
     a->info = info;
     a->hizq = hizq;
@@ -12,33 +14,41 @@ ArbolInt ArbolIntCons(int info, ArbolInt hizq, ArbolInt hder){
     return a;
 }
 
-void ArbolIntPreorden (ArbolInt a){
-    if (a != NULL){
+void ArbolIntPreorden (ArbolInt a)
+{
+    if (a != NULL)
+    {
         printf("  %d", a->info);
         ArbolIntPreorden(a->hizq);
         ArbolIntPreorden(a->hder);
     }
 }
 
-void ArbolIntOrden (ArbolInt a){
-    if (a != NULL){
+void ArbolIntOrden (ArbolInt a)
+{
+    if (a != NULL)
+    {
         ArbolIntOrden(a->hizq);
         printf(" %d", a->info);
         ArbolIntOrden(a->hder);
     }
 }
 
-void ArbolIntPosorden (ArbolInt a){
-    if (a != NULL){
+void ArbolIntPosorden (ArbolInt a)
+{
+    if (a != NULL)
+    {
         ArbolIntPosorden(a->hizq);
         ArbolIntPosorden(a->hder);
         printf(" %d", a->info);
     }
 }
 
-int ArbolIntCount (ArbolInt a){
+int ArbolIntCount (ArbolInt a)
+{
     int cont = 0;
-    if (a != NULL){
+    if (a != NULL)
+    {
         cont = 1;
         cont += ArbolIntCount(a->hizq);
         cont += ArbolIntCount(a->hder);
@@ -46,24 +56,35 @@ int ArbolIntCount (ArbolInt a){
     return cont;
 }
 
-bool ArbolIntPertenece (ArbolInt a, int valor){
+bool ArbolIntPertenece (ArbolInt a, int valor)
+{
     bool found = false;
-    while (a != NULL && found != true){
-        if (a->info == valor){
+    while (a != NULL && found != true)
+    {
+        if (a->info == valor)
+        {
             found = true;
-        }else
-            if (a->info > valor){
+        }
+        else
+        {
+            if (a->info > valor)
+            {
                 a = a->hizq;
-            }else
-                if (a->info < valor){
+            }
+            else
+                if (a->info < valor)
+                {
                     a = a->hder;
                 }
+        }
     }
     return found;
 }
 
-void ArbolIntDelete (ArbolInt a){
-    if (a != NULL){
+void ArbolIntDelete (ArbolInt a)
+{
+    if (a != NULL)
+    {
         ArbolIntDelete(a->hizq);
         ArbolIntDelete(a->hder);
         delete(a);
@@ -71,15 +92,18 @@ void ArbolIntDelete (ArbolInt a){
     a = NULL;
 }
 
-void ArbolIntCopy(ArbolInt &a, ArbolInt b){
-    if(b != NULL){
+void ArbolIntCopy(ArbolInt &a, ArbolInt b)
+{
+    if (b != NULL)
+    {
         a = ArbolIntCons(b->info, NULL, NULL);
         ArbolIntCopy(a->hizq, b->hizq);
         ArbolIntCopy(a->hder, b->hder);
     }
 }
 
-void ArbolIntAdd(ArbolInt &a, int valor){
+void ArbolIntAdd(ArbolInt &a, int valor)
+{
     if (a == NULL)
         a = ArbolIntCons(valor, NULL, NULL);
     else if (a->info > valor)
@@ -88,43 +112,63 @@ void ArbolIntAdd(ArbolInt &a, int valor){
         ArbolIntAdd(a->hder, valor);
 }
 
-int ArbolIntMin(ArbolInt a) {
+int ArbolIntMin(ArbolInt a)
+{
     while (a->hizq == NULL)
+    {
         a = a -> hizq;
+    }
     return (a->info);
 }
 
-void ArbolIntRemoveMin (ArbolInt &a){
+void ArbolIntRemoveMin (ArbolInt &a)
+{
     ArbolInt aux;
-    if (a->hizq == NULL){
+    if (a->hizq == NULL)
+    {
         aux = a->hder;
         delete a;
         a = aux;
-    }else
+    }
+    else
+    {
         ArbolIntRemoveMin (a->hizq);
+    }
 }
 
-void ArbolIntRemove (ArbolInt &a , int valor){
-    ArbolInt aux;
-    if (valor == a->info){
-        if (a->hder == NULL){
+void ArbolIntRemove (ArbolInt &a , int valor)
+{
+    if (a->info == valor)
+    {
+        ArbolInt aux;
+        if (a->hder == NULL)
+        {
             aux = a->hizq;
             delete a;
             a = aux;
-        }else
-            if (a->hizq == NULL){
-                aux = a->hder;
-                delete a;
-                a = aux;
-            }else{
-                a->info = ArbolIntMin (a->hder);
-                ArbolIntRemoveMin (a->hder);
-            }
-    }else{
-        if (valor < a->info)
-            ArbolIntRemove (a->hizq, valor);
+        }
+        else if (a->hizq == NULL)
+        {
+            aux = a->hder;
+            delete a;
+            a = aux;
+        }
         else
-            ArbolIntRemove (a->hder, valor);
+        {
+            a->info = ArbolIntMin(a->hder);
+            ArbolIntRemoveMin(a->hder);
+        }
+    }
+    else
+    {
+        if (valor < a->info)
+        {
+            ArbolIntRemove(a->hizq, valor);
+        }
+        else
+        {
+            ArbolIntRemove(a->hder, valor);
+        }
     }
 }
 
